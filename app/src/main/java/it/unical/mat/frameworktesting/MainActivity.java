0bd8696fb2fcb67754131092f9fc415bad8426f1
Jessica Zangari <jessica.zangari.90@gmail.com>
2016-05-17 13:01:41 +0200

    Changed the encoding load from resourses

--- app/src/main/java/it/unical/mat/frameworktesting/MainActivity.java
+++ app/src/main/java/it/unical/mat/frameworktesting/MainActivity.java
@@ -4,63 +4,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 import it.unical.mat.embasp.asp.AnswerSet;
 import it.unical.mat.embasp.asp.AnswerSets;
-import it.unical.mat.embasp.asp.IllegalTermException;
 import it.unical.mat.embasp.base.Callback;
 import it.unical.mat.embasp.base.Handler;
 import it.unical.mat.embasp.base.InputProgram;
-import it.unical.mat.embasp.base.OptionDescriptor;
 import it.unical.mat.embasp.base.Output;
-import it.unical.mat.embasp.base.Service;
 import it.unical.mat.embasp.platforms.android.AndroidHandler;
 import it.unical.mat.embasp.specializations.dlv.android.DLVAndroidService;
 
-
 public class MainActivity extends AppCompatActivity implements Callback{
 
+    public final int N=9;
+    private Handler handler;
+    private String encodingResource="sudoku";
+    private int [][] sudokuMatrix =     {{1,0,0,0,0,7,0,9,0},
+                                        {0,3,0,0,2,0,0,0,8},
+                                        {0,0,9,6,0,0,5,0,0},
+                                        {0,0,5,3,0,0,9,0,0},
+                                        {0,1,0,0,8,0,0,0,2},
+                                        {6,0,0,0,0,4,0,0,0},
+                                        {3,0,0,0,0,0,0,1,0},
+                                        {0,4,1,0,0,0,0,0,7},
+                                        {0,0,7,0,0,0,3,0,0}};
 
-    final int N=9;
-    final int M=9;
-    int [][]matrix={{1,0,0,0,0,7,0,9,0},
-                    {0,3,0,0,2,0,0,0,8},
-                    {0,0,9,6,0,0,5,0,0},
-                    {0,0,5,3,0,0,9,0,0},
-                    {0,1,0,0,8,0,0,0,2},
-                    {6,0,0,0,0,4,0,0,0},
-                    {3,0,0,0,0,0,0,1,0},
-                    {0,4,1,0,0,0,0,0,7},
-                    {0,0,7,0,0,0,3,0,0}};
-
-    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         handler = new AndroidHandler(getApplicationContext(), DLVAndroidService.class);
-        printMatrix();
+        printSudokuMatrix();
     }
 
-
-
-    String getResourceToString(String resourceName){
+    private String getEncodingFromResources(){
         InputStream ins = getResources().openRawResource(
-                getResources().getIdentifier(resourceName,
+                getResources().getIdentifier(encodingResource,
                         "raw", getPackageName()));
         BufferedReader reader=new BufferedReader(new InputStreamReader(ins));
         String line="";
@@ -75,34 +59,31 @@ public class MainActivity extends AppCompatActivity implements Callback{
         return builder.toString();
     }
 
-
-
     public void onClick(final View view){
-
-        String sudokuEncoding = getResourceToString("sudoku");
-
-        handler.addProgram(new InputProgram(sudokuEncoding));
         InputProgram inputProgram=new InputProgram();
         for ( int i = 0; i < N; i++)
-            for ( int j = 0; j < M; j++)
+            for ( int j = 0; j < N; j++)
                 try {
-                    if(matrix [ i ] [ j ]!=0) {
+                    if(sudokuMatrix[ i ] [ j ]!=0) {
                         inputProgram.addObjectInput(
-                                new Cell(i, j, matrix[i][j]));
+                                new Cell(i, j, sudokuMatrix[i][j]));
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
         handler.addProgram(inputProgram);
-        handler.addOption(new OptionDescriptor("-n=1"));
+
+        String sudokuEncoding = getEncodingFromResources();
+        handler.addProgram(new InputProgram(sudokuEncoding));
+
         handler.startAsync(this);
+
         Button button=(Button) findViewById(R.id.button);
         button.setEnabled(false);
     }
 
     @Override
     public void callback(final Output o) {
-
         if(!(o instanceof AnswerSets))return;
         AnswerSets answerSets=(AnswerSets)o;
         if(answerSets.getAnswersets().size()==0)return;
@@ -110,28 +91,25 @@ public class MainActivity extends AppCompatActivity implements Callback{
             try {
                 for(Object obj:as.getAtoms()) {
                     Cell cell = (Cell) obj;
-                    matrix[cell.getRow()][cell.getColumn()]=cell.getValue();
+                    sudokuMatrix[cell.getRow()][cell.getColumn()]=cell.getValue();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-        printMatrix();
-
+        printSudokuMatrix();
     }
 
-    private void printMatrix() {
+    private void printSudokuMatrix() {
         String out="";
         for ( int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                out += matrix[i][j]+" ";
+            for (int j = 0; j < N; j++) {
+                out += sudokuMatrix[i][j]+" ";
             }
             out+="\n";
-
         }
 
-
-        final String finalOut = out;
+        final String finalOut=out;
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
