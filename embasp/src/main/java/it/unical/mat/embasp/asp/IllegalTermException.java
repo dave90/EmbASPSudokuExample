5fe54899966887f9581c0558273cb9524592c0b8
dave90 <ddfusca@gmail.com>
2016-05-18 17:45:22 +0200

    Replace module with new aar of embasp 2.0

--- embasp/src/main/java/it/unical/mat/embasp/asp/IllegalTermException.java
+++
@@ -1,8 +0,0 @@
-package it.unical.mat.embasp.asp;
-
-public class IllegalTermException extends Exception {
-
-	public IllegalTermException(String msg) {
-		super(msg);
-	}
-}
--- embasp/src/main/java/it/unical/mat/embasp/specializations/dlv/DLVFilterOption.java
+++
@@ -1,9 +0,0 @@
-package it.unical.mat.embasp.specializations.dlv;
-
-import it.unical.mat.embasp.base.OptionDescriptor;
-
-public class DLVFilterOption extends OptionDescriptor {
-    public DLVFilterOption(String initial_option) {
-        super("-filter="+initial_option);
-    }
-}
